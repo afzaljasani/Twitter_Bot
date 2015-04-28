5f7cf37948265a2a0be2c82981a417f34f072e80
aj786123 <amj355@nyu.edu>
2015-04-28 02:16:44 -0400

    fix secrets

--- Xpath.py
+++ Xpath.py
@@ -7,10 +7,10 @@ import random
 import urllib2
 import time
 
-APP_KEY = os.getenv('TWITTER_APP_KEY')
-APP_SECRET = os.getenv('TWITTER_APP_SECRET')
-OAUTH_TOKEN = os.getenv('TWITTER_OAUTH_TOKEN')
-OAUTH_TOKEN_SECRET = os.getenv('TWITTER_OAUTH_TOKEN_SECRET')
+APP_KEY = os.getenv('APP_KEY')
+APP_SECRET = os.getenv('APP_SECRET')
+OAUTH_TOKEN = os.getenv('OAUTH_TOKEN')
+OAUTH_TOKEN_SECRET = os.getenv('OAUTH_TOKEN_SECRET')
 
 BASE_URL = "http://azlyrics.com/"
 ARTISTS_LIST = ["Nas", "Jayz", "ti"]
